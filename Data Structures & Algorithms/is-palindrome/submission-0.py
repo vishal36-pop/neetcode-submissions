0485bf93b49693eca2973c70932ad20fda6935d0
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-05-27 12:24:19 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = [i for i in s if i.isalnum()]
+        print(s)
+        l,r = 0,len(s)-1
+        while l<=r:
+            if s[l] == s[r]:
+                l+=1
+                r-=1
+            else :
+                return False
+        return True
