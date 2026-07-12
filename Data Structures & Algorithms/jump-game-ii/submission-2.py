e82afb696e0ade03757acb565f2d95268f2a24e9
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-07-12 18:49:19 +0530

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l = len(nums)
+        dp = [l for _ in range(l)]
+        #basecase is dp[l-1] = 0 > l-1 also 0
+        dp[l-1] = 0 
+        for i in range(l-2,-1,-1):
+            ans = l
+            for j in range(1,nums[i]+1):
+                if i+j>l-1:
+                    ans = min(ans,1+0)
+                    break
+                ans = min(ans,1+dp[i+j])
+            dp[i] = ans #if ans!= l else dp[i]
+        return dp[0]
