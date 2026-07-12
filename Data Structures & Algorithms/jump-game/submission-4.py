96178b677f4d50ffd866efa12800ee11577bffd9
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-07-12 17:03:30 +0530

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        #the greedy solution keep the jump which can reach the max dist 
+        #keep the jump means keep the reach of the index that is max until curr including curr
+        maxreach = 0
+        l = len(nums)
+        for i in range(len(nums)):
+            if i > maxreach:
+                return False
+            if i+nums[i] > maxreach:
+                maxreach = i+nums[i]
+                print(maxreach)
+            if maxreach >= l-1:
+                return True
+        return False
