8931f3ac3a69d02859976f316e176a22b949ffd2
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-07-23 12:30:01 +0530

    Add: open-the-lock - submission-4

---
+++ Data Structures & Algorithms/open-the-lock/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        
+        #here we should find the minimum distance to the target 
+        #when the nei = target
+        source = ['0000']
+        deadends = set(deadends)
+        visited = set()
+        visited.add('0000')
+        if '0000' in deadends:
+            return -1
+        queue = collections.deque(source)
+        
+        l = 0
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                curr = queue.popleft()
+                for i in range(4):
+                    digit = int(curr[i])
+                    for d in (1,-1):
+                        new_digit = str((digit+d)%10)
+                        new_state = curr[:i]+new_digit+curr[i+1:]
+                        if new_state in deadends:
+                            continue
+                        if new_state not in visited:
+                            if new_state == target:
+                                return l+1
+                            visited.add(new_state)
+                            queue.append(new_state)
+            l+=1
+        return -1
