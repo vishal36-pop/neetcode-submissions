2db7a7b93881585dcbb6393dae7d5c71f86a4871
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-07-24 11:49:54 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keystore = collections.defaultdict(list) 
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.keystore[key].append((timestamp,value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        # our key is the key 
+        #so now we have a sorted list of the timestamp,value accoring to timestamp
+        if self.keystore[key] == []:
+            return ''
+        if self.keystore[key][0][0] > timestamp:
+            return ""
+        l,r = 0,len(self.keystore[key])-1
+
+        while l<r:
+            mid = (l+r+1)//2
+            if self.keystore[key][mid][0] <= timestamp:
+                l = mid
+            else:
+                r = mid-1
+        return self.keystore[key][l][1]
