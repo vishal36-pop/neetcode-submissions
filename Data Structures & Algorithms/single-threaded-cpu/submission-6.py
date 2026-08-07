3c5baefb3f1c4358ceff5a305d3da5235c588643
Vishal Reddy Kondakindi <Vishal.Kondakindi@iiitb.ac.in>
2026-08-07 18:07:22 +0530

    Add: single-threaded-cpu - submission-6

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-6.py
@@ -0,0 +1,24 @@
+from _heapq import heappop
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        import heapq
+        from collections import deque
+        
+        q = sorted([(i,*j) for i,j in enumerate(tasks)],key = lambda x:x[1])
+        minheap = []
+        q = deque(q)
+        time = q[0][1] #start time is the first  #the start time of the first task
+        ans = []
+        while q or minheap :
+            while q and q[0][1] <= time:
+                i,_,p = q.popleft()
+                heapq.heappush(minheap,(p,i)) 
+            if minheap:
+                p,i = heapq.heappop(minheap)
+                ans.append(i)
+                time +=p
+            else:
+                time = q[0][1]
+        return ans
+            
+            
